e319d7a70bf40dcd32b08fc01cb1ef8053eb5dc8
Syedmoiz7 <syedzada7866@gmail.com>
2024-03-13 00:04:42 +0500

    redux toolkit setup

--- App.js
+++ App.js
@@ -4,60 +4,19 @@
  *
  * @format
  */
-
-import React from 'react';
-import { useState } from 'react';
-import { Text, View, Button, TextInput, Input } from 'react-native';
+ import React from "react";
+import { Provider } from 'react-redux';
 import CompanyData from './components/CompanyData'
-
-// let name = "Moiz"
-// let age = 22
-// let email = "abc@gmail.com"
-
-// function fruit() {
-//   return "apple"
-// }
-
+import store from './store';
 
 function App() {
-  const [name, setName] = useState("")
-  const [age, setAge] = useState("")
-  const [email, setEmail] = useState("")
-  const [test, setTest] = useState("12ehggauishjasd1")
-
-  const register = () => {
-    console.warn(name, age, email);
-    
-  }
-  const back = () => {
-    console.log('back');
-  }
   return (
-    // <View>
-    //   <Text style={{ fontSize: 30 }}>Hello React Native</Text>
-    //   <Button title="Press Me"></Button>
-    //   <Text>The Quick brown fox jumps over the lazy dog.</Text>
-    //   <Text>{name}</Text>
-    //   <Text>{email}</Text>
-    //   <Text>{fruit()}</Text>
-    //   <Text>{age === 22 ? "ok" : "unknown"}</Text>
-    //   <CompanyData/>
-    // </View>
-    <View>
-      <TextInput placeholder='Enter your name' style={{borderWidth: 1, margin: 5}} onChangeText={setName} value={name}></TextInput>
-      <TextInput placeholder='Enter your Age' style={{borderWidth: 1, margin: 5}} onChangeText={setAge} value={age}></TextInput>
-      <TextInput placeholder='Enter your email' style={{borderWidth: 1, margin: 5}} onChangeText={setEmail} value={email}></TextInput>
-      <TextInput placeholder='Testing' style={{borderWidth: 1, margin: 5}} onChangeText={setTest} value={test} ></TextInput>
-      <Button title='register' onPress={() =>{
-        register()
-        setName("") 
-        setAge("")
-        setEmail("")
-      }
-        } ></Button>
-      <Button title='back' color={'red'} onPress={back}></Button>
-    </View>
-  );
+    <Provider store={store}>
+        <CompanyData></CompanyData>
+    </Provider>
+  )
 }
+
+
 
 export default App;
--- components/CompanyData.js
+++ components/CompanyData.js
@@ -1,13 +1,46 @@
 import React from "react";
-import {Text, View} from "react-native";
+import { useState } from 'react';
+import { Text, View, Button, TextInput } from 'react-native';
 
 const CompanyData = () => {
+    // const [name, setName] = useState("")
+    // const [age, setAge] = useState("")
+    // const [email, setEmail] = useState("")
+    // const [test, setTest] = useState("12ehggauishjasd1")
+
+    // const register = () => {
+    //     console.warn(name, age, email);
+
+    // }
+    // const back = () => {
+    //     console.log('back');
+    // }
+
+    const addNewUser = () => {
+        
+    }
     return (
+
+        // <View>
+        //   <TextInput placeholder='Enter your name' style={{borderWidth: 1, margin: 5}} onChangeText={setName} value={name}></TextInput>
+        //   <TextInput placeholder='Enter your Age' style={{borderWidth: 1, margin: 5}} onChangeText={setAge} value={age}></TextInput>
+        //   <TextInput placeholder='Enter your email' style={{borderWidth: 1, margin: 5}} onChangeText={setEmail} value={email}></TextInput>
+        //   <TextInput placeholder='Testing' style={{borderWidth: 1, margin: 5}} onChangeText={setTest} value={test} ></TextInput>
+        //   <Button title='register' onPress={() =>{
+        //     register()
+        //     setName("") 
+        //     setAge("")
+        //     setEmail("")
+        //   }
+        //     } ></Button>
+        //   <Button title='back' color={'red'} onPress={back}></Button>
+        // </View>
+
         <View>
-            <Text style={{fontSize: 20}}>Name: XYZ Company </Text>
-            <Text  style={{fontSize: 20}}>Total employee: 500</Text>
+            <Button title='Add new users' onPress={addNewUser}></Button>
+            <Button title='Clear all users'></Button>
         </View>
-    )
+    );
 }
 
 export default CompanyData;
