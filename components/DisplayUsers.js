32f03ab16e163b2fc9567bf6a676d1bd81787285
Syedmoiz7 <syedzada7866@gmail.com>
2024-03-13 17:41:02 +0500

    deleting user

--- components/DisplayUsers.js
+++ components/DisplayUsers.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Text } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { Button, Text, View } from "react-native";
+import { removeUser } from "../store/slices/UserSlice";
+
 const DisplayUsers = () => {
     const data = useSelector((state) => {
         return state.users
     })
     console.log(data);
 
+    const dispatch = useDispatch()
+
+    const deleteUser = (id) => {
+        dispatch(removeUser(id))
+    }
+
     return (
         data.map((user, id) => {
             return <Text key={id}>
                 {user}
+                <View style={[{ width: "10%", marginTop: 10, backgroundColor: "red" }]}>
+                    <Button title="Delete User" onPress={() => deleteUser(id)}></Button>
+                </View>
             </Text>
         })
     )
